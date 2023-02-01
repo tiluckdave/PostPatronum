ccf35c751965f525226cd3814a7a06fc9287d4f0
Tilak Dave <davetilak003@gmail.com>
2023-02-02 01:58:08 +0530

    og values updated

--- pages/_document.js
+++ pages/_document.js
@@ -4,13 +4,8 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta property="og:title" content="GPT-3 Writer" key="title"/>
-        <meta property="og:description" content="build with buildspace" key="description"/>
-        <meta
-          property="og:image"
-          content="https://cdn.buildspace.so/courses/gpt3-writer/project-og.jpg"
-        />
-        <meta name="twitter:card" content="summary_large_image"></meta>
+        <meta property="og:title" content="Post Patronum" key="title" />
+        <meta property="og:description" content="LinkedIn post writer" key="description" />
       </Head>
       <body>
         <Main />
